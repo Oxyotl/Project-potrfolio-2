390a6c06f4f4bea386aa3a286535be5538c558ec
Oxyotl <cartemarano88@gmail.com>
2023-04-06 14:41:40 +0000

    Set up structure of game functions

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -1,1 +1,25 @@
 console.log("connected!")
+
+function runGame() {
+
+}
+
+// function generateRespond() {
+
+// }
+
+function checkAnswer() {
+
+}
+
+function incrementYourScore() {
+
+}
+
+function incrementPcScore() {
+
+}
+
+ function decreseTriesLeft() {
+    
+ }
