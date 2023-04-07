7fde0320433f6103bbf5426ba30348baf3a00fdc
Oxyotl <cartemarano88@gmail.com>
2023-04-07 10:17:12 +0000

    Correction eventListener for buttons, correction playGame function

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -10,26 +10,37 @@ const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+// document.addEventListener("DOMContentLoaded", function() {
+//     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons){ 
-        button.addEventListener("click", function(){
-            if (this.getElementsByClassName("btn") === "submit") {
-                alert("you clicked Submit!")
-            } else {
-                let gameType = this.getElementsByClassName("btn");
-                alert(`You clicked ${gameType}`)
-            }    
-        })
-    }
-})
-/**
- * The main part of the game, called after the user's answer has been processed
- */
-function runGame() {
-    let num1 = Math.floor(Math.random() * 5);
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    }); 
 }
+// if (this.getElementsByClassName("btn") === "submit") {
+//     alert("you clicked Submit!")
+//             } else {
+//                 let gameType = this.getElementsByClassName("btn");
+//                 alert(`You clicked ${gameType}`)
+//             }    
+//         })
+//     }
+// })
+/**
+ * The main part of the game, accepts the data-choice value of the selected button
+ */
+function playGame(playerChoice) {
+    
+    let computerChoice = Math.floor(Math.random() * 3)
+
+    let result = checkWinner(choices[computerchoice], choices[playerChoice]);
+
+    PaymentRequestUpdateEventScores(result);
+}
+
+
 
 // function generateRespond() {
 
@@ -47,6 +58,6 @@ function incrementPcScore() {
 
 }
 
- function decreseMovesLeft() {
+function decreseMovesLeft() {
 
- }
+}
