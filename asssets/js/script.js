51e63973653aa679cb9e647f80b3921e80d5ba36
Oxyotl <cartemarano88@gmail.com>
2023-04-07 09:16:25 +0000

    Addstyling for score-area and correction in footer style

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons){
+    for (let button of buttons){ 
         button.addEventListener("click", function(){
             if (this.getElementsByClassName("btn") === "submit") {
                 alert("you clicked Submit!")
